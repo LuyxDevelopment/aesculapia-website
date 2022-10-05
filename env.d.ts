6fcbcc8d0e19786caf610d7a98ad7872501f1566
decca-dev <medcherbani@gmail.com>
2022-10-05 22:00:24 +0100

    feat: more env types

--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,17 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       MONGO_URI: string;
+      COOKIE_NAME: string;
+      SESSION_PASSWORD: string;
+      SALT_ROUNDS: number;
     }
   }
 }
+
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: { email: string };
+  }
+}
+
+export {};
