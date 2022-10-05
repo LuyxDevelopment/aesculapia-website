f5310b9a8a6daa2cd6b673aaf20d0d55a5a9ede7
decca-dev <medcherbani@gmail.com>
2022-10-05 22:02:10 +0100

    feat: logout api route

---
+++ pages/api/auth/logout.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { ironOptions } from "../../../src/util/ironConfig";
+import { withIronSessionApiRoute } from "iron-session/next";
+
+export default withIronSessionApiRoute(function registerHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+): void {
+  req.session.destroy();
+  res.status(300).redirect("/");
+},
+ironOptions);
