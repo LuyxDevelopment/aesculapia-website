682dea72b972c8f12a2c96c06885d837b5b34561
Jaren Goldberg <fyrlex@gmail.com>
2022-10-06 14:22:49 -0400

    chore: remove user in place of admin

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,6 @@
+export * from './Admin.js';
 export * from './Event.js';
 export * from './Order.js';
 export * from './Product.js';
 export * from './Sponsor.js';
 export * from './Ticket.js';
-export * from './User.js';
