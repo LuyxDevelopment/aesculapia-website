2081a91a4cfc48f449774f45baca08364f81c8bb
decca-dev <medcherbani@gmail.com>
2022-10-07 22:51:11 +0100

    chore: routes

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -2,54 +2,80 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { Admin } from '../../../src/models/Admin';
 import dbConnect from '../../../src/util/dbConnect';
 import { genSalt, hash } from 'bcryptjs';
+import { ajv } from '../../../src/util/ajv';
 
 interface ResponseData {
 	error: boolean;
 	message: string;
 }
 
+interface RegisterNextApiRequest extends NextApiRequest {
+	body: {
+		email: string;
+		password: string;
+	};
+	method: 'POST';
+}
+
 dbConnect();
 
+const validateRegisterBody = ajv.compile<RegisterNextApiRequest['body']>({
+	email: {
+		type: 'string',
+	},
+	password: {
+		type: 'string',
+	},
+});
+
 export default async function registerHandler(
-	req: NextApiRequest,
+	req: RegisterNextApiRequest,
 	res: NextApiResponse<ResponseData>,
 ): Promise<void> {
-	if (req.method === 'POST') {
-		const { email, password } = req.body as { email: string; password: string };
-		const existingEmail = await Admin.findOne({ email: email });
-		if (existingEmail) {
-			res.status(400).json({
-				error: true,
-				message: 'Email already in use!',
+	switch (req.method) {
+		case 'POST': {
+			validateRegisterBody(req.body);
+
+			const email = req.body.email;
+			const password = req.body.password;
+			const existingEmail = await Admin.findOne({ email: email });
+
+			if (existingEmail) {
+				res.status(400).json({
+					error: true,
+					message: 'Email already in use!',
+				});
+				return;
+			}
+
+			const admin = new Admin({
+				email: email,
 			});
-			return;
+
+			genSalt(process.env.SALT_ROUNDS, (saltError, salt) => {
+				if (saltError) throw new Error(saltError.message);
+
+				hash(password, salt, async (hashError, hash) => {
+					if (hashError) throw new Error(hashError.message);
+
+					try {
+						admin.password = hash;
+						await admin.save();
+
+						res.status(200).json({
+							error: false,
+							message: 'Admin profile created successfully!',
+						});
+					} catch (error: any) {
+						res.status(400).json({
+							error: true,
+							message: `Failed to register admin due to the following error: ${error?.message}`,
+						});
+
+						throw new Error(error.message);
+					}
+				});
+			});
 		}
-		const admin = new Admin({
-			email: email,
-		});
-		genSalt(process.env.SALT_ROUNDS, (saltError, salt) => {
-			if (saltError) throw new Error(saltError.message);
-
-			hash(password, salt, async (hashError, hash) => {
-				if (hashError) throw new Error(hashError.message);
-
-				try {
-					admin.password = hash;
-					await admin.save();
-
-					res.status(200).json({
-						error: false,
-						message: 'Admin profile created successfully!',
-					});
-				} catch (error: any) {
-					res.status(400).json({
-						error: true,
-						message: `Failed to register admin due to the following error: ${error?.message}`,
-					});
-
-					throw new Error(error.message);
-				}
-			});
-		});
 	}
 }
