f38390ca287a853d41b799a6f496f4585e11a03e
53P <53p.playz@gmail.com>
2022-10-10 11:19:56 -0600

    feat(api): add products route
    Co-authored-by: Fyrlex <fyrlex@gmail.com>

--- pages/api/products.ts
+++ pages/api/products.ts
@@ -1,8 +1,111 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { Admin, Product } from '../../src/models';
 
-export default function productsHandler(
+export default async function productsHandler(
 	req: NextApiRequest,
 	res: NextApiResponse,
-): void {
-	return void res.status(200);
+): Promise<void> {
+	switch (req.method) {
+		case 'DELETE': {
+			const admin = await Admin.findOne({ email: req.session.user?.email });
+
+			if (!admin) {
+				return void res.status(403).json({
+					error: true,
+					message: 'Forbidden access',
+				});
+			}
+
+			const product = await Product.findByIdAndDelete(req.query.pid);
+
+			if (!product) {
+				return void res.status(404).json({
+					error: true,
+					message: 'Product not found.',
+				});
+			}
+
+			return void res.status(200).json({
+				error: false,
+				message: `Product #${req.query.pid} successfully deleted.`,
+			});
+		}
+
+		case 'GET': {
+			if (req.query.pid) {
+				const product = await Product.findById(req.query.pid);
+
+				if (!product) {
+					return void res.status(404).json({
+						error: true,
+						message: 'Product not found.',
+					});
+				}
+
+				return void res.status(200).json({
+					error: false,
+					message: 'Product found.',
+					data: product,
+				});
+			}
+
+			const products = await Product.find({});
+
+			return void res.status(200).json({
+				error: false,
+				message: 'All products found',
+				data: products,
+			});
+		}
+
+		case 'PATCH': {
+			const admin = await Admin.findOne({ email: req.session.user?.email });
+
+			if (!admin) {
+				return void res.status(403).json({
+					error: true,
+					message: 'Forbidden access',
+				});
+			}
+
+			const product = await Product.findByIdAndUpdate(req.query.pid, { $set: req.body });
+
+			if (!product) {
+				return void res.status(404).json({
+					error: true,
+					message: 'Product not found.',
+				});
+			}
+
+			return void res.status(200).json({
+				error: false,
+				message: 'Product successfully updated',
+				data: product,
+			});
+		}
+
+		case 'POST': {
+			const admin = await Admin.findOne({ email: req.session.user?.email });
+
+			if (!admin) {
+				return void res.status(403).json({
+					error: true,
+					message: 'Forbidden access',
+				});
+			}
+
+			const product = new Product(req.body);
+
+			try {
+				await product.save();
+			} catch (error) {
+				console.log(error);
+
+				return void res.status(400).json({
+					error: true,
+					message: 'Error creating product',
+				});
+			}
+		} break;
+	}
 }
