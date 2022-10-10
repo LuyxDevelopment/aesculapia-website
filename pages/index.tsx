3c494fbc3671a38a212a7e589dd728d00306b2ad
53P <53p.playz@gmail.com>
2022-10-10 11:22:32 -0600

    feat(pages): remove powered by message

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,26 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 
 export default function Home(): JSX.Element {
 	return (
-		<div className={styles.container}>
+		<div>
 			<Head>
 				<title>Aesculapia</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
-			<main className={styles.main}>
-				<h1 className={styles.title}>
+			<main >
+				<h1 >
 					Aesculapia
 				</h1>
 			</main>
 
-			<footer className={styles.footer}>
+			<footer >
 				<a
 					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					Powered by{' '}
-					<span className={styles.logo}>
-						<Image src="/vercel.svg" alt="Luyx Logo" width={72} height={16} />
-					</span>
 				</a>
 			</footer>
 		</div>
