c27fbabc5901f1ad493967e29a92a436c77d155d
Fyrlex <fyrlex@gmail.com>
2022-10-18 10:35:59 -0400

    remove old files

--- pages/api/home.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(
-	req: NextApiRequest,
-	res: NextApiResponse,
-): void {
-	return res.status(200).send();
-}
