2e255778d7f8d8b2b9df94dc5bbb05e63b7b22e1
53P <53p.playz@gmail.com>
2022-10-22 10:54:23 -0600

    feat(pages): implement redirect to force user to be authenticated

--- pages/admin/sponsors.tsx
+++ pages/admin/sponsors.tsx
@@ -1,9 +1,66 @@
-import type { NextPage } from 'next';
+import { withIronSessionSsr } from 'iron-session/next';
+import { ironOptions } from '../../src/util/ironConfig';
 
-const AdminSponsors: NextPage = () => {
-	return (
-		<></>
-	);
-};
+function AdminSponsors(): JSX.Element {
+	return (<></>);
+}
+
+// @ts-ignore
+export const getServerSideProps = withIronSessionSsr(async function ({ req }) {
+	const user = req.session.user;
+
+	if (user?.email) {
+		const request = await fetch('http://localhost:3000/api/auth/2fa/generate', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(user),
+		});
+	
+		const json = await request.json();
+
+		if (json.data) return {
+			props: { 
+				user: { email: user.email, has2faEnabled: true, completed2fa: false },
+				otpAuthUri: json.data,
+			},
+		};
+		return {
+			props: { 
+				user: { email: user.email, has2faEnabled: false, completed2fa: false },
+				otpAuthUri: '',
+			},
+		};
+	}
+
+	if (!user) {
+		return {
+			props: {
+				user: { email: '', has2faEnabled: false, completed2fa: false },
+			},
+			redirect: {
+				destination: '/admin/login',
+				permanent: false,
+			},
+		};
+	}
+
+	if (!user.has2faEnabled) {
+		return {
+			props: {
+				user: { email: user.email, has2faEnabled: false, completed2fa: false },
+			},
+			redirect: {
+				destination: '/admin/settings',
+				permanent: false,
+			},
+		};
+	}
+
+	return {
+		props: { user: req.session.user },
+	};
+}, ironOptions);
 
 export default AdminSponsors;
