07c8225d4afb37bc16f025fb8f1bfac9df17aa1a
53P <53p.playz@gmail.com>
2022-10-22 11:00:17 -0600

    feat(components): add alt text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import type { FC } from 'react';
 
 const Footer: FC = () => {
@@ -8,30 +7,31 @@ const Footer: FC = () => {
 			<div className="flex flex-row justify-around pb-3">
 				<Image
 					src="/assets/logo.png"
+					alt="Logo"
 					width="100"
 					height="100"
 				/>
 				<div className="felx flex-col">
-					<h1 className="f-header">Penises</h1>
-					<h2>lol</h2>
-					<h2>lol</h2>
-					<h2>lol</h2>
-					<h2>lol</h2>
+					<h1 className="f-header">1</h1>
+					<h2>1</h2>
+					<h2>2</h2>
+					<h2>3</h2>
+					<h2>4</h2>
 				</div>
 				<div className="felx flex-col">
-					<h1 className="f-header">Penises</h1>
-					<h2>lol</h2>
-					<h2>lol</h2>
-					<h2>lol</h2>
-					<h2>lol</h2>
+					<h1 className="f-header">2</h1>
+					<h2>1</h2>
+					<h2>2</h2>
+					<h2>3</h2>
+					<h2>4</h2>
 				</div>
 			</div>
 			<div className="py-8 flex flex-row items-center justify-center space-x-5">
-				<h1>dickord</h1>
-				<h1>dickord</h1>
-				<h1>dickord</h1>
+				<h1>1</h1>
+				<h1>2</h1>
+				<h1>3</h1>
 			</div>
-			<h1 className="text-center pb-6">aesculpia inc penis</h1>
+			<h1 className="text-center pb-6">Aesculapia</h1>
 		</footer>
 	);
 };
