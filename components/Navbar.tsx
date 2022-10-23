ca36635f7d84eeb6b95be49eef8324216b2c0b57
decca-dev <medcherbani@gmail.com>
2022-10-23 23:06:15 +0100

    feat: responsive navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,13 +3,15 @@ import Image from 'next/image';
 
 const Navbar: FC = () => {
 	return (
-		<nav className="bg-white text-black shadow-lg p-3 flex flex-row justify-evenly">
+		<nav className="bg-white text-black shadow-lg p-5 flex flex-row sm:justify-evenly justify-between">
 			<Image
 				src="/assets/logo.png"
 				width="70"
 				height="70"
+				onClick={() => (window.location.href = '/')}
 			/>
-			<div className="flex flex-col">
+			<span className="sm:hidden block float-right">hamburger</span>
+			<div className="hidden sm:block">
 				<h1>Heading</h1>
 				<div>
 					<h2>peace</h2>
@@ -17,7 +19,7 @@ const Navbar: FC = () => {
 					<h2>peace</h2>
 				</div>
 			</div>
-			<div className="flex flex-col">
+			<div className="hidden sm:block">
 				<h1>Heading</h1>
 				<div>
 					<h2>peace</h2>
@@ -25,15 +27,7 @@ const Navbar: FC = () => {
 					<h2>peace</h2>
 				</div>
 			</div>
-			<div className="flex flex-col">
-				<h1>Heading</h1>
-				<div>
-					<h2>peace</h2>
-					<h2>peace</h2>
-					<h2>peace</h2>
-				</div>
-			</div>
-			<div className="flex flex-col">
+			<div className="hidden sm:block">
 				<h1>Heading</h1>
 				<div>
 					<h2>peace</h2>
