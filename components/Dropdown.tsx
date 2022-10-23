893db6a5fc804f56353d12897c068a704de0af7b
decca-dev <medcherbani@gmail.com>
2022-10-23 21:57:00 +0100

    style: dropdown format

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -13,7 +13,11 @@ interface DropdownProps {
 	items: DropdownItem[];
 }
 
-const Dropdown = ({ direction, children, items }: DropdownProps): JSX.Element => {
+const Dropdown = ({
+	direction,
+	children,
+	items,
+}: DropdownProps): JSX.Element => {
 	const dropdownRef = useRef(null);
 	const [isVisible, setIsVisible] = useState(false);
 	const toggleVisibility = (): void => setIsVisible(!isVisible);
@@ -21,13 +25,14 @@ const Dropdown = ({ direction, children, items }: DropdownProps): JSX.Element =>
 
 	return (
 		<div
-			className={`${direction === 'right'
-				? 'flex flex-row'
-				: direction === 'left'
+			className={`${
+				direction === 'right'
+					? 'flex flex-row'
+					: direction === 'left'
 					? 'flex flex-row-reverse'
 					: direction === 'bottom'
-						? 'relative inline-block'
-						: ''
+					? 'relative inline-block'
+					: ''
 			}`}
 			ref={dropdownRef}
 		>
@@ -35,7 +40,13 @@ const Dropdown = ({ direction, children, items }: DropdownProps): JSX.Element =>
 			{isVisible && (
 				<div className="absolute flex flex-col rounded-md drop-shadow-lg child-xl cursor-pointer">
 					{items.map((item, i) => {
-						return <DropdownItem href={item.href} text={item.text} key={i} />;
+						return (
+							<DropdownItem
+								href={item.href}
+								text={item.text}
+								key={i}
+							/>
+						);
 					})}
 				</div>
 			)}
@@ -48,7 +59,7 @@ const DropdownItem = ({ text, href }: DropdownItem): JSX.Element => {
 		<>
 			<p
 				className="py-2 w-28 sm:w-32 pl-2 border-gray-200 border-b-2 bg-white text-black"
-				onClick={():string => (window.location.href = href)}
+				onClick={(): string => (window.location.href = href)}
 			>
 				<Link href={href}>{text}</Link>
 			</p>
