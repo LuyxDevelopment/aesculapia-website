477b36d40fbd9890c091a03696bf4a0084b0c7d5
decca-dev <medcherbani@gmail.com>
2022-10-26 22:02:45 +0100

    refactor: useMetaData hook uses og image gen api

--- lib/hooks/useMetaData.tsx
+++ lib/hooks/useMetaData.tsx
@@ -1,10 +1,21 @@
 import Head from 'next/head';
+import { useState, useEffect } from 'react';
 
 export const useMetaData = (
 	title: string,
 	description: string,
 	url: string,
 ): JSX.Element => {
+	const [image, setImage] = useState('');
+	useEffect(() => {
+		fetch(`/api/og?description=${description}`).then((res) => {
+			if (res.ok) {
+				setImage(res.url);
+			} else {
+				setImage('/assets/logo.png');
+			}
+		});
+	}, []);
 	return (
 		<Head>
 			<title>{title} | Aesculapia</title>
@@ -14,11 +25,27 @@ export const useMetaData = (
 				type="image/png"
 			/>
 
-			<meta property="og:title" content={title} />
-			<meta property="og:description" content={description} />
-			<meta property="og:url" content={url} />
-			<meta property="og:image" content="/assets/favicon.ico" />
-			<meta content="#4297BA" data-react-helmet="true" name="theme-color" />
+			<meta
+				property="og:title"
+				content={title}
+			/>
+			<meta
+				property="og:description"
+				content={description}
+			/>
+			<meta
+				property="og:url"
+				content={url}
+			/>
+			<meta
+				property="og:image"
+				content={image}
+			/>
+			<meta
+				content="#4297BA"
+				data-react-helmet="true"
+				name="theme-color"
+			/>
 		</Head>
 	);
 };
