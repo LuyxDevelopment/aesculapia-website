04872c9a93edc0f6ee231a6be288fa8d40b811f3
decca-dev <medcherbani@gmail.com>
2022-11-04 22:30:23 +0100

    navbar done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,50 +2,65 @@
 
 import { FC } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
 
 const Navbar: FC = () => {
+	const [showMenu, setShowMenu] = useState(false);
+
 	return (
-		<nav className="bg-white text-black shadow-lg p-5 flex flex-row sm:justify-evenly justify-between">
-			<Image
-				src="/assets/logo.png"
-				width="70"
-				height="70"
-				onClick={(): string => (window.location.href = '/')}
-				alt="Logo"
-			/>
-			<span className="sm:hidden block float-right">
+		<>
+			<nav className="bg-white text-black shadow-lg p-5 flex flex-row justify-between sm:justify-evenly items-center">
 				<Image
-					src="/assets/icons/hamburger.svg"
-					width={160}
-					height={87}
-					alt="Navbar"
+					src="/assets/logo.png"
+					width="70"
+					height="70"
+					alt="Logo"
+					onClick={() => (window.location.href = '/')}
 				/>
-			</span>
-			<div className="hidden sm:block">
-				<h1>Heading</h1>
-				<div>
-					<h2>peace</h2>
-					<h2>peace</h2>
-					<h2>peace</h2>
+				<div className="hidden sm:flex sm:flex-row sm:space-x-10 font-extrabold">
+					<h1 className="cursor-pointer hover:underline transition-all duration-300 ease-in-out">
+						Song
+					</h1>
+					<h1 className="cursor-pointer hover:underline transition-all duration-300 ease-in-out">
+						Products
+					</h1>
+					<h1 className="cursor-pointer hover:underline transition-all duration-300 ease-in-out">
+						Calendar
+					</h1>
+					<h1 className="cursor-pointer hover:underline transition-all duration-300 ease-in-out">
+						Sponsors
+					</h1>
 				</div>
-			</div>
-			<div className="hidden sm:block">
-				<h1>Heading</h1>
-				<div>
-					<h2>peace</h2>
-					<h2>peace</h2>
-					<h2>peace</h2>
+				<span
+					className="block float-right sm:hidden cursor-pointer hover:scale-90 transition-all duration-300 ease-in-out"
+					onClick={() => setShowMenu(!showMenu)}
+				>
+					<Image
+						src="/assets/icons/hamburger.svg"
+						width={35}
+						height={35}
+						alt="Navbar"
+					/>
+				</span>
+			</nav>
+			{showMenu && (
+				<div className="animate-translate_top flex flex-col items-center justify-center sm:hidden bg-gray-200 shadow-xl p-3 font-extrabold mt-5">
+					<h1 className="cursor-pointer hover:underline transition-all duration-300 ease-in-out">
+						Song
+					</h1>
+					<h1 className="cursor-pointer hover:underline transition-all duration-300 ease-in-out">
+						Products
+					</h1>
+					<h1 className="cursor-pointer hover:underline transition-all duration-300 ease-in-out">
+						Calendar
+					</h1>
+					<h1 className="cursor-pointer hover:underline transition-all duration-300 ease-in-out">
+						Sponsors
+					</h1>
 				</div>
-			</div>
-			<div className="hidden sm:block">
-				<h1>Heading</h1>
-				<div>
-					<h2>peace</h2>
-					<h2>peace</h2>
-					<h2>peace</h2>
-				</div>
-			</div>
-		</nav>
+			)}
+		</>
 	);
 };
 
