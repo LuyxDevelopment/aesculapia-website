8155bc00bafad09683f62dc2864bad4483aaae97
decca-dev <medcherbani@gmail.com>
2022-11-04 22:30:34 +0100

    tailwind config modifications

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,21 @@ module.exports = {
 		'./components/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				translate_top: {
+					'0%': {
+						translate: '0px -20px',
+					},
+					'100%': {
+						transform: '0px',
+					},
+				},
+			},
+			animation: {
+				translate_top: 'translate_top 1s ease-in-out',
+			},
+		},
 	},
 	plugins: [],
 };
