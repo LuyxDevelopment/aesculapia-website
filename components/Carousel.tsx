1dc3851518fd1a5f0336b9f8a3135fc6c7b99239
decca-dev <medcherbani@gmail.com>
2022-11-06 00:23:14 +0100

    fix carousel

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -1,4 +1,4 @@
-import { useState, FC } from 'react';
+import { useState, FC, useEffect } from 'react';
 import Image from 'next/image';
 
 export interface ImageData {
@@ -7,20 +7,14 @@ export interface ImageData {
 	url: string;
 }
 
-export interface CarouselProps {
-	images: ImageData[];
-	height: string;
-}
-
-const Carousel: FC<{ props: CarouselProps }> = ({ props }) => {
+const Carousel: FC<{ images: ImageData[] }> = ({ images }) => {
 	const [current, setCurrent] = useState(0);
 
-	const next = () => setCurrent((current + 1) % props.images.length);
-	const prev = () =>
-		setCurrent((current - 1 + props.images.length) % props.images.length);
+	const next = () => setCurrent((current + 1) % images.length);
+	const prev = () => setCurrent((current - 1 + images.length) % images.length);
 
 	return (
-		<div className="py-5 flex flex-row items-stretch justify-center h-full text-white">
+		<div className="py-5 flex flex-row items-stretch justify-center text-white">
 			<button
 				onClick={prev}
 				className="text-4xl font-bold bg-red-400 w-12 rounded-l-lg"
@@ -28,9 +22,9 @@ const Carousel: FC<{ props: CarouselProps }> = ({ props }) => {
 				{'<'}
 			</button>
 			<img
-				src={props.images[current].url}
+				src={images[current].url}
 				width="90%"
-				height={props.height}
+				height="100vh"
 			/>
 			<button
 				onClick={prev}
