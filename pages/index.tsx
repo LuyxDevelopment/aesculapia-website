1926610f76cce3a75097513f3b07c7c55b411d02
decca-dev <medcherbani@gmail.com>
2022-11-06 00:23:28 +0100

    some homepage stuff and animate on scroll

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,37 +2,62 @@ import Layout from '../components/Layout';
 import { useMetaData } from '../lib/hooks/useMetaData';
 import type { NextPage } from 'next';
 import Carousel, { ImageData } from '../components/Carousel';
+import { useEffect } from 'react';
 
 const images: ImageData[] = [
 	{
 		name: 'hello',
 		id: 1,
-		url: 'https://cdn.discordapp.com/emojis/881231953876107295.png?quality=lossless',
+		url: 'http://localhost:3000/api/og?description=man',
 	},
 	{
 		name: 'hello',
 		id: 2,
-		url: 'https://cdn.discordapp.com/emojis/871536829516644352.png',
+		url: 'http://localhost:3000/api/og?description=manandwomen',
 	},
 	{
 		name: 'hello',
 		id: 3,
-		url: 'https://cdn.discordapp.com/attachments/913973764830658620/1019645714646704158/20220914_102753.jpg',
-	},
-	{
-		name: 'hello',
-		id: 4,
-		url: 'https://cdn.discordapp.com/attachments/913973764830658620/1016714902234206370/unknown.png',
+		url: 'http://localhost:3000/api/og?description=manandwomenandkids',
 	},
 ];
 
 const Home: NextPage = () => {
+	useEffect(() => {
+		const observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					entry.target.classList.add('show-aos');
+				} else {
+					entry.target.classList.remove('show-aos');
+				}
+			});
+		});
+		const hidden = document.querySelectorAll('.hidden-aos');
+		hidden.forEach((el) => observer.observe(el));
+	});
 	return (
 		<>
 			{useMetaData('Aesculapia', 'Home Page', '/')}
 			<Layout>
-				<div style={{ width: '100%', height: '100vh' }}>
-					<Carousel images={images} />
+				<div className="relative">
+					<div className="absolute top-10 left-14 sm:left-20 text-white">
+						<div className="flex flex-wrap w-96">
+							<h1 className="text-3xl pb-2 font-bold">
+								Welcome to the website of Aesculapia!
+							</h1>
+							<p className="text-xl">
+								The student's association for medical students at the University
+								of Antwrep.
+							</p>
+						</div>
+					</div>
+					<div className="h-[100vh]">
+						<Carousel images={images} />
+					</div>
+				</div>
+				<div className="mt-6 hidden-aos">
+					<h1>About us</h1>
 				</div>
 			</Layout>
 		</>
