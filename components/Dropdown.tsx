e244a187984a57e48b05b19d4641f513c3f6a539
decca-dev <medcherbani@gmail.com>
2022-11-06 20:02:13 +0100

    feat: Dropdown is more customizable

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,22 +1,29 @@
-import { useState, useRef, ReactNode } from 'react';
+import { useState, useRef, ReactNode, CSSProperties } from 'react';
 import Link from 'next/link';
 import { useOutsideAlerter } from '../lib/hooks/useOutsideAlerter';
 
 interface DropdownItem {
 	text: string;
-	href: string;
+	href?: string;
+	style?: CSSProperties;
 }
 
 interface DropdownProps {
 	direction: 'right' | 'left' | 'bottom';
 	children: ReactNode;
 	items: DropdownItem[];
+	newSpace?: boolean;
+	style?: CSSProperties;
+	itemStyle?: CSSProperties;
 }
 
 const Dropdown = ({
 	direction,
 	children,
 	items,
+	newSpace,
+	style,
+	itemStyle,
 }: DropdownProps): JSX.Element => {
 	const dropdownRef = useRef(null);
 	const [isVisible, setIsVisible] = useState(false);
@@ -29,22 +36,28 @@ const Dropdown = ({
 				direction === 'right'
 					? 'flex flex-row'
 					: direction === 'left'
-						? 'flex flex-row-reverse'
-						: direction === 'bottom'
-							? 'relative inline-block'
-							: ''
+					? 'flex flex-row-reverse'
+					: direction === 'bottom'
+					? 'relative inline-block'
+					: ''
 			}`}
 			ref={dropdownRef}
 		>
 			<button onClick={toggleVisibility}>{children}</button>
 			{isVisible && (
-				<div className="absolute flex flex-col rounded-md drop-shadow-lg child-xl cursor-pointer">
+				<div
+					className={`${
+						newSpace ? 'relative' : 'absolute'
+					} flex flex-col rounded-md drop-shadow-lg child-xl cursor-pointer`}
+					style={style}
+				>
 					{items.map((item, i) => {
 						return (
 							<DropdownItem
 								href={item.href}
 								text={item.text}
 								key={i}
+								style={itemStyle}
 							/>
 						);
 					})}
@@ -54,14 +67,22 @@ const Dropdown = ({
 	);
 };
 
-const DropdownItem = ({ text, href }: DropdownItem): JSX.Element => {
+const DropdownItem = ({ text, href, style }: DropdownItem): JSX.Element => {
 	return (
 		<>
 			<p
-				className="py-2 w-28 sm:w-32 pl-2 border-gray-200 border-b-2 bg-white text-black"
-				onClick={(): string => (window.location.href = href)}
+				className="py-2 w-28 flex flex-wrap pl-2 border-gray-200 border-b-2 bg-white text-black"
+				onClick={
+					href !== undefined
+						? (): string => (window.location.href = href)
+						: () => {
+								return false;
+						  }
+				}
+				style={style}
 			>
-				<Link href={href}>{text}</Link>
+				{href !== undefined && <Link href={href!}>{text}</Link>}
+				{href === undefined && <p>{text}</p>}
 			</p>
 		</>
 	);
