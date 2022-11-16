d6568b9f710b4a58c9f3d8c912be49416076994b
53P <53p.playz@gmail.com>
2022-11-16 07:51:09 -0700

    feat(components): fix css on ShoppingCartItem

--- components/ShoppingCartItem.tsx
+++ components/ShoppingCartItem.tsx
@@ -10,13 +10,16 @@ const ShoppingCartItem: FC<Props> = ({ item }) => {
 
 	return <>
 		<div className='flex'>
-			<img className='w-24 sm:w-52 md:w-64 rounded-lg' draggable={false} src={item.imageURL} alt={item.name} width='64' height='64' />
-			<div className='flex-col'>
+			<img className='w-24 sm:w-52 md:w-64 rounded-lg mt-2 mb-2' draggable={false} src={item.imageURL} alt={item.name} width='64' height='64' />
+			<div className='flex-col mt-2 mb-2 ml-2'>
 				<h1 className='text-xl font-bold'>{item.name}</h1>
 				<p>€{(item.price / item.amount / 100).toFixed(2)} each</p>
 			</div>
-			<div className='flex-col sm:absolute sm:right-96'>
+			<div className='flex-col ml-auto mr-0 mt-2 mb-2'>
 				<p className='text-lg'>€{(item.price / 100).toFixed(2)}</p>
+			</div>
+			<div>
+				<a></a>
 			</div>
 		</div>
 		{/* <div className='group bg-gray-200 p-4 rounded-lg shadow-md hover:scale-[1.04] transiton-all duration-[400ms] ease-in-out relative'>
---
+++ pages/cart.tsx
@@ -0,0 +1,52 @@
+import { NextPage } from 'next';
+import { IProduct } from '../src/models';
+import { useMetaData } from '../lib/hooks/useMetaData';
+import Layout from '../components/Layout';
+import { useEffect, useState } from 'react';
+import ShoppingCartItem from '../components/ShoppingCartItem';
+import { Product } from '../components/ProductCard';
+
+interface Props {
+	data: (IProduct & { _id: string; })[];
+}
+
+const ShoppingCart: NextPage<Props> = ({ data }) => {
+	const [cart, setCart] = useState([]);
+	const [price, setPrice] = useState(0);
+
+	useEffect(() => {
+		if (typeof window === undefined) return;
+		setCart(JSON.parse(window.localStorage.getItem('cart')!));
+		cart.forEach((item: Product) => setPrice(prevPrice => prevPrice + item.price));
+	}, []);
+
+	return (
+		<>
+			{useMetaData('Shopping Cart', 'Shopping Cart Page', '/cart')}
+			<Layout>
+				{!cart && (
+					<p>There are no products in the cart.</p>
+				)}
+				{cart && (
+					<>
+						<div className="container flex flex-row">
+							<div className='mb-12'>
+								<h1 className='text-4xl font-bold mb-5'>Shopping Cart</h1>
+								<div className='grid grid-cols-1 divide-y'>
+									{cart.map((item, i) => {
+										return <ShoppingCartItem item={item} key={i}></ShoppingCartItem>;
+									})}
+								</div>
+							</div>
+							<div className=''>
+								<p>{(price / 100).toFixed(2)}</p>
+							</div>
+						</div>
+					</>
+				)}
+			</Layout>
+		</>
+	);
+};
+
+export default ShoppingCart;
