88347fa634ea5abd90a0da6a008005c69ea3800f
53P <53p.playz@gmail.com>
2022-11-16 16:17:49 -0700

    fix: possible fix for bad gateway error

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,11 +1,11 @@
 import { NextPage, NextPageContext } from 'next';
-import ProductCard from '../../components/ProductCard';
-import { IProduct, ProductDocument } from '../../src/models/Product';
-import { useMetaData } from '../../lib/hooks/useMetaData';
-import Layout from '../../components/Layout';
-import ErrorPage from '../../components/Error';
-import { useLocalStorage } from '../../lib/hooks/useLocalStorage';
-import { BaseProps, ResponseData } from '../../src/types/index';
+import ProductCard from '../components/ProductCard';
+import { IProduct, ProductDocument } from '../src/models/Product';
+import { useMetaData } from '../lib/hooks/useMetaData';
+import Layout from '../components/Layout';
+import ErrorPage from '../components/Error';
+import { useLocalStorage } from '../lib/hooks/useLocalStorage';
+import { BaseProps, ResponseData } from '../src/types/index';
 
 interface Props {
 	data: (IProduct & { _id: string; })[];
@@ -45,7 +45,7 @@ export const getServerSideProps = async ({ res }: NextPageContext): Promise<Base
 		'public, s-maxage=10, stale-while-revalidate=59',
 	);
 
-	const request = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/products`, {
+	const request = await fetch('/api/products', {
 		method: 'GET',
 		headers: { 'Content-Type': 'application/json' },
 	});
