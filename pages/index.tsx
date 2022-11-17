6655ad740fb83bfee7b8bfd6552a84f29e366a62
decca-dev <medcherbani@gmail.com>
2022-11-17 22:31:13 +0100

    removed AOS that was causing trouble

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import type { NextPage } from 'next';
 import Layout from '../components/Layout';
 import { useMetaData } from '../lib/hooks/useMetaData';
 import Carousel, { ImageData } from '../components/Carousel';
-import AOS from '../components/AOS';
 import Dropdown from '../components/Dropdown';
 
 const images: ImageData[] = [
@@ -53,114 +52,110 @@ const Index: NextPage = () => {
 		<>
 			{useMetaData('Aesculapia | Home', 'Home Page', '/')}
 			<Layout>
-				<div className='relative'>
-					<div className='absolute top-10 left-14 sm:left-20 text-black'>
-						<div className='flex flex-wrap w-56 sm:w-96'>
-							<h1 className='text-5xl pb-6 font-bold'>
+				<div className="relative">
+					<div className="absolute top-10 left-14 sm:left-20 text-black">
+						<div className="flex flex-wrap w-56 sm:w-96">
+							<h1 className="text-5xl pb-6 font-bold">
 								Welkom op de website van Aesculapia!
 							</h1>
-							<p className='text-xl'>
+							<p className="text-xl">
 								De studentenvereniging voor geneeskundestudenten aan de
 								Universiteit Antwerpen.
 							</p>
 						</div>
 					</div>
-					{/* <div>
+					<div>
 						<Carousel
 							images={images}
 							fullScreen={true}
 						/>
-					</div> */}
+					</div>
 				</div>
-				<div className='container mt-24'>
-					<AOS dir='rl'>
-						<div className='my-6 flex flex-col sm:flex-row justify-between items-center'>
-							<div>
-								<h1 className='font-semibold text-4xl'>
-									Wie zijn wij?
-								</h1>
-								<p className='italic'>
-									We zijn een groep studenten met zoveel talent!
-								</p>
-							</div>
-							<div className='px-5'></div>
+				<div className="container mt-24">
+					<div className="my-6 flex flex-col sm:flex-row justify-between items-center">
+						<div>
+							<h1 className="font-semibold text-4xl">Wie zijn wij?</h1>
+							<p className="italic">
+								We zijn een groep studenten met zoveel talent!
+							</p>
+						</div>
+						<div className="px-5"></div>
+						<Image
+							src={
+								process.env.NEXT_PUBLIC_DOMAIN +
+								'/api/og?description=group%20photo%201'
+							}
+							width={1200}
+							height={630}
+							className="w-60 sm:w-72"
+							alt="Alt"
+						/>
+					</div>
+					<div className="mt-20">
+						<div className="flex flex-col-reverse sm:flex-row justify-between items-center">
 							<Image
-								src={process.env.NEXT_PUBLIC_DOMAIN + '/api/og?description=group%20photo%201'}
+								src={
+									process.env.NEXT_PUBLIC_DOMAIN +
+									'/api/og?description=group%20photo%202'
+								}
 								width={1200}
 								height={630}
-								className='w-60 sm:w-72'
-								alt='Alt'
+								className="w-60 sm:w-72"
+								alt="Alt"
 							/>
+							<div className="px-5"></div>
+							<div>
+								<h1 className="font-semibold text-4xl">
+									Waar streven we naar?
+								</h1>
+								<p className="italic">De wereld verbeteren!</p>
+							</div>
 						</div>
-					</AOS>
-					<div className='mt-20'>
-						<AOS dir='lr'>
-							<div className='flex flex-col-reverse sm:flex-row justify-between items-center'>
-								<Image
-									src={process.env.NEXT_PUBLIC_DOMAIN + '/api/og?description=group%20photo%202'}
-									width={1200}
-									height={630}
-									className='w-60 sm:w-72'
-									alt='Alt'
-								/>
-								<div className='px-5'></div>
-								<div>
-									<h1 className='font-semibold text-4xl'>
-										Waar streven we naar?
-									</h1>
-									<p className='italic'>
-										De wereld verbeteren!
-									</p>
-								</div>
-							</div>
-						</AOS>
 					</div>
-					<div className='mt-32 mb-32'>
-						<AOS dir='tb'>
-							<h1 className='text-center text-4xl font-bold mb-5'>
-								Komende evenementen
-							</h1>
-							<div className='flex flex-col sm:flex-row items-center justify-evenly mb-5 text-white'>
-								{events.map((event, i) => {
-									return (
-										<div key={i}>
-											<Dropdown
-												direction='bottom'
-												items={[
-													{
-														text: event.description,
-													},
-													{
-														text: `Ends at: ${new Date(
-															event.endsAtTimestamp,
-														).toDateString()}`,
-													},
-													{
-														text: 'Take me there!',
-														href: `/events/${event._id}`,
-														tw: 'text-red-500',
-													},
-												]}
-												newSpace={true}
-												tw='pl-14'
+					<div className="mt-32 mb-32">
+						<h1 className="text-center text-4xl font-bold mb-5">
+							Komende evenementen
+						</h1>
+						<div className="flex flex-col sm:flex-row items-center justify-evenly mb-5 text-white">
+							{events.map((event, i) => {
+								return (
+									<div key={i}>
+										<Dropdown
+											direction="bottom"
+											items={[
+												{
+													text: event.description,
+												},
+												{
+													text: `Ends at: ${new Date(
+														event.endsAtTimestamp,
+													).toDateString()}`,
+												},
+												{
+													text: 'Take me there!',
+													href: `/events/${event._id}`,
+													tw: 'text-red-500',
+												},
+											]}
+											newSpace={true}
+											tw="pl-14 mb-5"
+										>
+											<div
+												className="flex flex-col items-center justify-center w-56 h-24 hover:scale-110 hover:-translate-y-2 transition-all duration-300 ease-in-out cursor-pointer mb-3"
+												style={{
+													backgroundImage: `url(${event.banner})`,
+													backgroundRepeat: 'no-repeat',
+													backgroundSize: '100% 100%',
+													backgroundPosition: 'center',
+												}}
 											>
-												<div
-													className='flex flex-col items-center justify-center w-56 h-24 hover:scale-110 hover:-translate-y-2 transition-all duration-300 ease-in-out cursor-pointer mb-3'
-													style={{
-														backgroundImage: `url(${event.banner})`,
-														backgroundRepeat: 'no-repeat',
-														backgroundSize: '100% 100%',
-														backgroundPosition: 'center',
-													}}
-												>
-													<h1 className='font-bold text-xl'>{event.title}</h1>
-												</div>
-											</Dropdown>
-										</div>
-									);
-								})}
-							</div>
-						</AOS>
+												<h1 className="font-bold text-xl">{event.title}</h1>
+											</div>
+										</Dropdown>
+									</div>
+								);
+							})}
+						</div>
 					</div>
 				</div>
 			</Layout>
