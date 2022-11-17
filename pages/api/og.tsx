41e23b0f17ca6961824677ae59c4cec8d4f80ed8
decca-dev <medcherbani@gmail.com>
2022-11-17 23:33:13 +0100

    wrapped og image generation inside try-catch

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -6,36 +6,44 @@ export const config = {
 };
 
 export default function ogHandler(req: NextRequest): ImageResponse {
-	const { searchParams } = req.nextUrl;
-	const description = searchParams.get('description') ?? 'Aesculapia';
+	try {
+		const { searchParams } = req.nextUrl;
+		const description = searchParams.get('description') ?? 'Aesculapia';
 
-	return new ImageResponse(
-		(
-			<div
-				style={{
-					display: 'flex',
-					flexDirection: 'column',
-					color: 'white',
-					alignItems: 'center',
-					justifyContent: 'center',
-					background: 'linear-gradient(to left, #ef4444, #991b1b)',
-					width: '100%',
-					height: '100%',
-					borderRadius: 25,
-				}}
-			>
-				<img
-					src={`${process.env.NEXT_PUBLIC_DOMAIN}/assets/logo.png`}
-					alt='Aesculapia Logo'
-					width='150'
-					tw='pb-3'
-				/>
-				<p tw='text-3xl font-bold'>{description}</p>
-			</div>
-		),
-		{
-			width: 1200,
-			height: 630,
-		},
-	);
+		return new ImageResponse(
+			(
+				<div
+					style={{
+						display: 'flex',
+						flexDirection: 'column',
+						color: 'white',
+						alignItems: 'center',
+						justifyContent: 'center',
+						background: 'linear-gradient(to left, #ef4444, #991b1b)',
+						width: '100%',
+						height: '100%',
+					}}
+				>
+					<img
+						src={`${process.env.NEXT_PUBLIC_DOMAIN}/assets/logo.png`}
+						alt="Aesculapia Logo"
+						width="150"
+						tw="pb-3"
+					/>
+					<p tw="text-3xl font-bold">{description}</p>
+				</div>
+			),
+			{
+				width: 1200,
+				height: 630,
+			},
+		);
+	} catch (error: any) {
+		console.log(
+			`An error occured trying to generate OG image: ${error.message}`,
+		);
+		return new Response(`Failed to generate the image`, {
+			status: 500,
+		});
+	}
 }
