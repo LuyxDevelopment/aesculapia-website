ea29c6a7810226ea1893901f3122c28b1ca4bd0d
decca-dev <medcherbani@gmail.com>
2022-11-17 23:19:11 +0100

    made praesidium page

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,9 +1,31 @@
 import { FC } from 'react';
+import Image from 'next/image';
 
+export interface UserProfile {
+	name: string;
+	role: string;
+	photo: string;
+}
 
-const Profile: FC = () => {
+interface Props {
+	profile: UserProfile;
+}
+
+const Profile: FC<Props> = ({ profile }) => {
 	return (
-		<div>Profile</div>
+		<div className="flex h-72 w-52 flex-col items-center justify-center rounded-md bg-gradient-to-r from-orange-400 to-red-500 shadow-xl transition-all duration-500 ease-in-out hover:-translate-y-3 hover:scale-90">
+			<div className="rounded-md">
+				<Image
+					src={profile.photo}
+					width="175"
+					height="175"
+				/>
+			</div>
+			<h1 className="text-xl text-center pt-3 font-bold">{profile.name}</h1>
+			<p className="text-lg mt-1 font-semibold italic text-gray-300">
+				{profile.role}
+			</p>
+		</div>
 	);
 };
 
---
+++ pages/praesidium.tsx
@@ -0,0 +1,104 @@
+import type { NextPage } from 'next';
+import { useMetaData } from '../lib/hooks/useMetaData';
+import Layout from '../components/Layout';
+import Profile, { UserProfile } from '../components/Profile';
+
+const people: UserProfile[] = [
+	{
+		name: 'Lena Ceulemans',
+		role: 'Praeses',
+		photo: 'http://localhost:3000/api/og?description=foto',
+	},
+	{
+		name: 'Margot Dellaert',
+		role: 'Vice-praeses',
+		photo: 'http://localhost:3000/api/og?description=foto',
+	},
+	{
+		name: 'Floris Van Hemel',
+		role: 'Quaestor',
+		photo: 'http://localhost:3000/api/og?description=foto',
+	},
+	{
+		name: 'Noa van Mook',
+		role: 'Cantor',
+		photo: 'http://localhost:3000/api/og?description=foto',
+	},
+	{
+		name: 'Siebe Van Poeyer',
+		role: 'Schachtentemmer',
+		photo: 'http://localhost:3000/api/og?description=foto',
+	},
+	{
+		name: 'Dries Van der Paal',
+		role: 'Zedenmeester',
+		photo: 'http://localhost:3000/api/og?description=foto',
+	},
+	{
+		name: 'Victor Van den Bosch',
+		role: 'Feest',
+		photo: 'http://localhost:3000/api/og?description=foto',
+	},
+	{
+		name: 'Timo Dobbeleir',
+		role: 'Feest',
+		photo: 'http://localhost:3000/api/og?description=foto',
+	},
+	{
+		name: 'Charlotte Breems',
+		role: 'Cultuur',
+		photo: 'http://localhost:3000/api/og?description=foto',
+	},
+	{
+		name: 'Wout Van Opdenbosch',
+		role: 'Media',
+		photo: 'http://localhost:3000/api/og?description=foto',
+	},
+	{
+		name: 'Stan Reijnen',
+		role: 'P.R',
+		photo: 'http://localhost:3000/api/og?description=foto',
+	},
+	{
+		name: 'Fynn Hille',
+		role: 'Sport',
+		photo: 'http://localhost:3000/api/og?description=foto',
+	},
+	{
+		name: 'Evy Janssens',
+		role: 'Sport',
+		photo: 'http://localhost:3000/api/og?description=foto',
+	},
+	{
+		name: 'Adriana Dane',
+		role: 'Scriptor',
+		photo: 'http://localhost:3000/api/og?description=foto',
+	},
+	{
+		name: 'Ilsa Bons',
+		role: 'Mentor',
+		photo: 'http://localhost:3000/api/og?description=foto',
+	},
+];
+
+const praesidium: NextPage = () => {
+	return (
+		<>
+			{useMetaData('Praesidium', 'Praesidium', '/praesidium')}
+			<Layout>
+				<div className="my-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 place-items-center space-y-3">
+					{people.map((person, i) => {
+						return (
+							<Profile
+								profile={person}
+								key={i}
+							/>
+						);
+					})}
+				</div>
+			</Layout>
+		</>
+	);
+};
+
+export default praesidium;
