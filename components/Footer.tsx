83a93e798b70061a7b1da64b45e0dc47c8d30faa
decca-dev <medcherbani@gmail.com>
2022-11-18 15:53:48 +0100

    use Image with local files br

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,20 @@
-/* eslint-disable @next/next/no-img-element */
 import type { FC } from 'react';
+import Image from 'next/image';
 
 const Footer: FC = () => {
 	return (
-		<footer className='flex flex-col p-10 bg-red-500 text-white'>
-			<div className='flex flex-row justify-around pb-3'>
-				<img
-					src='/assets/logo.png'
-					alt='Logo'
-					width='100'
-					height='100'
+		<footer className="flex flex-col p-10 bg-red-500 text-white">
+			<div className="flex flex-row justify-around pb-3">
+				<Image
+					src="/assets/logo.png"
+					alt="Logo"
+					width="100"
+					height="100"
 				/>
-				<div className='felx flex-col'>
-					<h1 className='text-xl font-bold hover:no-underline transition ease-in-out duration-150'>Over ons</h1>
+				<div className="felx flex-col">
+					<h1 className="text-xl font-bold hover:no-underline transition ease-in-out duration-150">
+						Over ons
+					</h1>
 					<h2>Email:</h2>
 					<h2>Contact:</h2>
 					<h2>Info:</h2>
@@ -20,41 +22,41 @@ const Footer: FC = () => {
 					<h2>Ondernemingsnummer: </h2>
 				</div>
 			</div>
-			<div className='py-8 flex flex-row items-center justify-center'>
-				<span className='px-1'>
-					<img
-						src='/assets/icons/facebook.svg'
+			<div className="py-8 flex flex-row items-center justify-center">
+				<span className="px-1">
+					<Image
+						src="/assets/icons/facebook.svg"
 						width={50}
 						height={50}
-						alt='Facebook'
+						alt="Facebook"
 					/>
 				</span>
-				<span className='px-1'>
-					<img
-						src='/assets/icons/linkedin.svg'
+				<span className="px-1">
+					<Image
+						src="/assets/icons/linkedin.svg"
 						width={50}
 						height={50}
-						alt='LinkedIn'
+						alt="LinkedIn"
 					/>
 				</span>
-				<span className='px-1'>
-					<img
-						src='/assets/icons/youtube.svg'
+				<span className="px-1">
+					<Image
+						src="/assets/icons/youtube.svg"
 						width={50}
 						height={50}
-						alt='YouTube'
+						alt="YouTube"
 					/>
 				</span>
-				<span className='px-1'>
-					<img
-						src='/assets/icons/instagram.svg'
+				<span className="px-1">
+					<Image
+						src="/assets/icons/instagram.svg"
 						width={50}
 						height={50}
-						alt='Instagram'
+						alt="Instagram"
 					/>
 				</span>
 			</div>
-			<h1 className='text-center pb-6'>Aesculapia</h1>
+			<h1 className="text-center pb-6">Aesculapia</h1>
 		</footer>
 	);
 };
