67ef3688db3f6eada52380d81155dbdf9f59594c
Jaren Goldberg <fyrlex@gmail.com>
2023-09-23 15:40:35 -0400

    fix

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -62,33 +62,9 @@ const AdminIndex: NextPage = () => {
 	);
 };
 
+// eslint-disable-next-line require-await
 export const getServerSideProps = withIronSessionSsr(async function ({ req }): Promise<AdminProps> {
 	const user = req.session.user;
-
-	if (user?.email) {
-		const request = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/auth/2fa/generate`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(user),
-		});
-
-		const json = await request.json();
-
-		if (json.data) return {
-			props: {
-				user: { email: user.email, has2faEnabled: true, completed2fa: false },
-				otpAuthUri: json.data,
-			},
-		};
-		return {
-			props: {
-				user: { email: user.email, has2faEnabled: false, completed2fa: false },
-				otpAuthUri: '',
-			},
-		};
-	}
 
 	if (!user) {
 		return {
@@ -97,18 +73,6 @@ export const getServerSideProps = withIronSessionSsr(async function ({ req }): P
 			},
 			redirect: {
 				destination: '/admin/login',
-				permanent: false,
-			},
-		};
-	}
-
-	if (!user.has2faEnabled) {
-		return {
-			props: {
-				user: { email: user.email, has2faEnabled: false, completed2fa: false },
-			},
-			redirect: {
-				destination: '/admin/settings',
 				permanent: false,
 			},
 		};
